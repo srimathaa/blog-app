9ebaff3df7695ab2fa47828ceb4ca37a3161c14a
Srimatha25 <srimathasrimi85@gmail.com>
2026-07-28 12:05:08 +0530

    Add missing getPostById export to api/posts.js

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -2,6 +2,7 @@ import api from './axios';
 
 export const getPosts = (params = {}) => api.get('/posts', { params });
 export const getPostBySlug = (slug) => api.get(`/posts/${slug}`);
+export const getPostById = (id) => api.get(`/posts/id/${id}`);
 export const createPost = (data) => api.post('/posts', data);
 export const updatePost = (id, data) => api.put(`/posts/${id}`, data);
 export const deletePost = (id) => api.delete(`/posts/${id}`);
